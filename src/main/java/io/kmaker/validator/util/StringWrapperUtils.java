5a5f50c37cf78ed13e80dac70c01e0fd40f565d1
TOUCH Yong An <yongan.touch@kmaker.io>
2026-02-06 09:35:19 +0700

    Convert into gradle project

--- src/main/java/io/kmaker/validator/util/StringWrapperUtils.java
+++ src/main/java/io/kmaker/validator/util/StringWrapperUtils.java
@@ -1,6 +1,7 @@
 package io.kmaker.validator.util;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.Strings;
 
 public final class StringWrapperUtils {
 
@@ -21,31 +22,22 @@ public final class StringWrapperUtils {
 
     public static boolean equals(final String s1,
                                  final String s2) {
-        return StringUtils.equals(s1, s2);
+        return Strings.CS.equals(s1, s2);
     }
 
     public static boolean equalsIgnoreCase(final String s1,
                                            final String s2) {
-        return StringUtils.equalsIgnoreCase(s1, s2);
+        return Strings.CI.equals(s1, s2);
     }
 
     public static boolean endsWith(final String s1,
                                    final String s2) {
-        return StringUtils.endsWith(s1, s2);
-    }
-
-    public static boolean endsWithIgnoreCase(final String s1,
-                                             final String s2) {
-        return StringUtils.endsWithIgnoreCase(s1, s2);
+        return Strings.CS.endsWith(s1, s2);
     }
 
     public static boolean startsWith(final String s1,
                                      final String s2) {
-        return StringUtils.startsWith(s1, s2);
+        return Strings.CS.startsWith(s1, s2);
     }
 
-    public static boolean startsWithIgnoreCase(final String s1,
-                                               final String s2) {
-        return StringUtils.startsWithIgnoreCase(s1, s2);
-    }
 }
